547cafa01788f4d748bd537fb17e87fbd95f9a26
Yuka Matsuo <matsuo.yk@gmail.com>
2017-07-25 21:32:50 +0900

    #018解答

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using Base.Test
 
-for i in 1:17
+for i in 1:18
     include("test_problem$(@sprintf("%03d", i)).jl")
 end
---
+++ test/test_problem018.jl
@@ -0,0 +1,6 @@
+using Base.Test
+using ProjectEuler
+
+println("Test Problem0018: ")
+println("-- main")
+@test Problem018.solve() == 1074
